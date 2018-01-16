36a2d1f05aac1de91cf63efedf17cb2d142aef94
hupin <sylvie.hupin@sfr.fr>
2018-01-16 11:16:35 +0100

    init angularjs

---
+++ app/scripts/app.js
@@ -0,0 +1,12 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name quickSurveyFrontendApp
+ * @description
+ * # quickSurveyFrontendApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('quickSurveyFrontendApp', []);
