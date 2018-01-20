6d2b81274f6653250019b0930d484be77898366f
Sylvie HUPIN <sylvie.hupin@sfr.fr>
2018-01-20 22:06:45 +0100

    premier ecran - creation enquete

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -8,10 +8,17 @@
  * Controller of the quickSurveyFrontendApp
  */
 angular.module('quickSurveyApp')
-  .controller('MainCtrl', function () {
+  .controller('MainCtrl', [ '$scope', function ($scope) {
     this.awesomeThings = [
       'HTML5 Boilerplate',
       'AngularJS',
       'Karma'
     ];
-  });
+
+    $scope.enquetes = [
+      {"libelle": "enquete 1"},
+      {"libelle": "enquete 2"}
+    ];
+    
+    
+  }]);
