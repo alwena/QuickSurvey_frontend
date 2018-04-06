12e49cf40d6cf69ad2c605040a6984d577a943cf
Wioma <jm.kervern@gmail.com>
2018-04-06 21:20:59 +0200

    personnes

--- app/scripts/controllers/listeSonde.js
+++ app/scripts/controllers/listeSonde.js
@@ -28,10 +28,35 @@ angular.module('quickSurveyApp')
 
      var init = function() {
 
-
-
-
      }
+
+    $scope.terminer = function () {
+
+      //creation liste de personnes sondees
+           var enqueteJson = sessionStorage.getItem('enquete');
+           var enquete = JSON.parse(enqueteJson);
+
+           $scope.listePersonnes = {};
+           $scope.listePersonnes.personnes=[];
+
+           var url = 'http://localhost/QuickSurvey_backend/web/app_dev.php/api/survey/'+enquete.id+'/persons';
+
+            var listeSondes = $resource(url);
+
+            $scope.listePersonnes.personnes = $scope.personnes;
+
+            $scope.personnes = listeSondes.save($scope.listePersonnes);
+
+            $scope.personnes.$promise.then(function (result) {
+
+                  if ($scope.personnes.message != undefined) {
+                        $window.alert($scope.personnes.message);
+                  } else {
+                  console.log(result);
+                     $location.path('/accueil');
+                  }
+           });
+    }
 
    $scope.ajouter = function () {
      var newPersonne = {
